0159ce2fe66b80b4134cdd4f6d1d617e4bee3b3b
sreiswig <sam.reiswig@gmail.com>
2025-01-09 22:08:19 -0600

    setup

---
+++ devenv.nix
@@ -0,0 +1,58 @@
+{ pkgs, lib, config, inputs, ... }:
+let buildInputs = with pkgs; [
+  cudaPackages.cudatoolkit
+];
+in
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [ cudaPackages.cudatoolkit
+                          libGL
+                          gcc12 
+                        ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  env = {
+    LD_LIBRARY_PATH = "${
+      lib.makeLibraryPath buildInputs
+    }:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+    CUDA_PATH = pkgs.cudaPackages.cudatoolkit;
+  };
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
